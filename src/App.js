fa7ef86523fe1bf5f32b75b55396927dcf17d4a1
Kirtesh Bansal <kirtesh.bansal@siply.in>
2021-09-25 10:27:27 +0530

    Render Translate component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import Container from "@material-ui/core/Container";
+
+import Translate from "./components/Translate";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+import theme from "./theme/theme";
+import "./public/css/styles.css";
 
 const App = () => {
-    return <></>;
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Container maxWidth="md">
+                    <ErrorBoundary>
+                        <Translate />
+                    </ErrorBoundary>
+                </Container>
+            </ThemeProvider>
+        </>
+    );
 };
 
 export default App;
